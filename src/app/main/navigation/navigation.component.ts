a1c79b7f1f1af7a6b5a1bb198aa4467a646b5407
balacarter <30482570+balacarter@users.noreply.github.com>
2020-08-31 14:29:33 -0700

    Added scroll directive/update me page

--- src/app/main/navigation/navigation.component.ts
+++ src/app/main/navigation/navigation.component.ts
@@ -1,40 +1,16 @@
-import { Component, OnInit, AfterViewInit, ViewChild, ElementRef, HostListener } from '@angular/core';
+import { Component} from '@angular/core';
 
 @Component({
   selector: 'app-navigation',
   templateUrl: './navigation.component.html',
   styleUrls: ['./navigation.component.scss']
 })
-export class NavigationComponent implements OnInit{
+export class NavigationComponent{
   navLinks = [
     { path: 'me', label: 'About Me' },
     { path: 'actnow', label: 'ACTnow' }
   ];
 
-  @ViewChild('stickyMenu') menuElement: ElementRef;
-
-  sticky: boolean = false;
-  elementPosition: any;
-
-  constructor() { }
-
-  ngOnInit() {
-  }
-
-  ngAfterViewInit(){
-    this.elementPosition = this.menuElement.nativeElement.offsetTop;
-    console.log(this.elementPosition.offsetTop);
-  }
-
-  @HostListener('window:scroll', ['$event'])
-  handleScroll(){
-    const windowScroll = window.pageYOffset;
-    if(windowScroll > this.elementPosition){
-      console.log(this.elementPosition.offsetTop);
-      this.sticky = true;
-    } else {
-      this.sticky = false;
-    }
-  }
+  
 
 }

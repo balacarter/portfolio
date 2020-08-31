e797ce646ef2843d5a3ce2476ac65473c15e9535
balacarter <30482570+balacarter@users.noreply.github.com>
2020-08-31 11:15:59 -0700

    l

--- src/app/main/navigation/navigation.component.ts
+++ src/app/main/navigation/navigation.component.ts
@@ -1,16 +1,40 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterViewInit, ViewChild, ElementRef, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-navigation',
   templateUrl: './navigation.component.html',
   styleUrls: ['./navigation.component.scss']
 })
-export class NavigationComponent {
+export class NavigationComponent implements OnInit{
   navLinks = [
     { path: 'me', label: 'About Me' },
     { path: 'actnow', label: 'ACTnow' }
   ];
 
+  @ViewChild('stickyMenu') menuElement: ElementRef;
 
+  sticky: boolean = false;
+  elementPosition: any;
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  ngAfterViewInit(){
+    this.elementPosition = this.menuElement.nativeElement.offsetTop;
+    console.log(this.elementPosition.offsetTop);
+  }
+
+  @HostListener('window:scroll', ['$event'])
+  handleScroll(){
+    const windowScroll = window.pageYOffset;
+    if(windowScroll > this.elementPosition){
+      console.log(this.elementPosition.offsetTop);
+      this.sticky = true;
+    } else {
+      this.sticky = false;
+    }
+  }
 
 }
